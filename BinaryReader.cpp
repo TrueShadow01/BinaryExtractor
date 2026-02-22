a5043dddaeb60070fca1e0a6b97dede4467cc337
Shadow <94984080+TrueShadow01@users.noreply.github.com>
2026-02-22 02:28:46 +0100

    Implemented BinaryReader::Tell()

--- BinaryReader.cpp
+++ BinaryReader.cpp
@@ -23,10 +23,27 @@ uint32_t BinaryReader::ReadUInt32() {
 	return value;
 }
 
+// Moves the File Pointer to a specific offset
 void BinaryReader::Seek(size_t offset) {
+	// move file pointer from the beginning of the file to the offest
 	m_file.seekg(offset, std::ios::beg);
+	// error checking
 	if (!m_file) {
 		std::cerr << "Failed to move File pointer!" << std::endl;
 		exit(1);
 	}
+}
+
+// Shows you where the file pointer is currently
+size_t BinaryReader::Tell() {
+	// get file pointer position
+	std::streampos pos = m_file.tellg();
+
+	// check if file pointer position is valid
+	if (pos == std::streampos(-1)) {
+		std::cerr << "Failed to get file position!" << std::endl;
+		exit(1);
+	}
+
+	return static_cast<size_t>(pos);
 }
